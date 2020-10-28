74b3cecbfc323aac75bcf2ad0a4c1cb81f774adc
devho813 <devho813@gmail.com>
2020-10-28 18:05:35 +0900

    1. next.js 10버전으로 업그레이드 2. next-compose-plugins 추가 3. next-transpile-modules 추가

--- next.config.js
+++ next.config.js
@@ -1,21 +1,15 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const path = require('path');
+const withPlugins = require('next-compose-plugins');
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
+const withTM = require('next-transpile-modules')([
+  // 트랜스파일 필요한 모듈 추가
+]);
 
-module.exports = withBundleAnalyzer({
+module.exports = withPlugins([withBundleAnalyzer, withTM], {
   webpack: function (config) {
-    const originalEntry = config.entry;
-
-    config.entry = async () => {
-      const entries = await originalEntry();
-      if (entries['main.js'] && !entries['main.js'].includes('./polyfills.js')) {
-        entries['main.js'].unshift('./polyfills.js');
-      }
-      return entries;
-    };
-
     config.resolve.alias = {
       ...config.resolve.alias,
       '~': path.resolve(__dirname, './src'),
