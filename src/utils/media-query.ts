9c051d2b9d9bc3c1095aaf0101c062cf822613fa
devho813 <devho813@gmail.com>
2021-01-17 01:30:16 +0900

    미디어 쿼리 break-point 수정

--- src/utils/media-query.ts
+++ src/utils/media-query.ts
@@ -1,18 +1,25 @@
 import { css } from 'styled-components';
 
 type BreakPoint = {
-  desktop: number;
   tablet: number;
+  Laptop: number;
+  LaptopL: number;
 };
 
+// mobile: 375px
+// tablet: 768px
+// Laptop: 1024px
+// Laptop L: 1440px
 export const sizes: BreakPoint = {
-  desktop: 1366,
   tablet: 768,
+  Laptop: 1024,
+  LaptopL: 1440,
 };
 
 type Media = {
-  desktop: (arg: TemplateStringsArray) => ReturnType<typeof css>;
   tablet: (arg: TemplateStringsArray) => ReturnType<typeof css>;
+  Laptop: (arg: TemplateStringsArray) => ReturnType<typeof css>;
+  LaptopL: (arg: TemplateStringsArray) => ReturnType<typeof css>;
 };
 
 const media: Media = Object.keys(sizes).reduce((acc: any, label: string) => {
